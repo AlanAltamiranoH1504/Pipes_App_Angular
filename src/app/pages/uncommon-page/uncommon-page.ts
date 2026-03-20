94252cba5c9af80bbd3c887625342fa51838c761
Alan Altamirano Hernández <alan.altamirano@viveplus.com.mx>
2026-03-20 11:32:43 -0600

    feat: add support for internationalization pipes, slice, JSON, and key-value pairs

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../../components/card-component/card-component';
+import { JsonPipe, SlicePipe } from '@angular/common';
+
+const client1 = {
+  name: 'Alan',
+  gender: 'male',
+  age: 24,
+  address: 'CDMX',
+};
+
+const client2 = {
+  name: 'Vanessa',
+  gender: 'female',
+  age: 23,
+  address: 'EDOMEX',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [CardComponent, SlicePipe, JsonPipe],
   templateUrl: './uncommon-page.html',
   styleUrl: './uncommon-page.css',
 })
 export class UncommonPage {
+  // i18n Select
+  protected client = signal(client1);
 
+  protected changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+    } else {
+      this.client.set(client1);
+    }
+  }
+
+  // i18n Plural
+  protected clients = signal([
+    "Maria", 
+    "Pedro",
+    "Fernado",
+    "Juan",
+    "Gustavo",
+    "Alejanddra",
+    "Miguel"
+  ]);
+
+  protected deleteClient() {
+    this.clients.update((client) => client.slice(1));
+  }
 }
